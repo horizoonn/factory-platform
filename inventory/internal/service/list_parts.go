e421c58fdeafc65058764e95049dd47bc0f5c60e
horizon <iliasyunusov@yandex.ru>
2026-05-15 11:03:36 +0500

    feat(inventory): add service layer

---
+++ inventory/internal/service/list_parts.go
@@ -0,0 +1,17 @@
+package service
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/horizoonn/factory-platform.git/inventory/internal/domain"
+)
+
+func (s *InventoryService) ListParts(ctx context.Context, filter domain.PartsFilter) ([]domain.Part, error) {
+	parts, err := s.inventoryRepository.ListParts(ctx, filter)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get parts from repository: %w", err)
+	}
+
+	return parts, nil
+}
