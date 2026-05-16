df0ec1d949c63d9447d20910cb5927355a83ad89
horizon <iliasyunusov@yandex.ru>
2026-05-16 16:30:32 +0500

    feat(inventory): add part postgres repository

---
+++ inventory/internal/repository/part/get.go
@@ -0,0 +1,36 @@
+package part
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/google/uuid"
+
+	"github.com/horizoonn/factory-platform.git/inventory/internal/domain"
+	postgrespool "github.com/horizoonn/factory-platform.git/platform/pkg/database/postgres/pool"
+)
+
+func (r *Repository) GetPart(ctx context.Context, id uuid.UUID) (domain.Part, error) {
+	ctx, cancel := context.WithTimeout(ctx, r.pool.OpTimeout())
+	defer cancel()
+
+	query := `
+	SELECT id, name, description, price, stock_quantity, category, dimensions,
+		manufacturer, tags, metadata, created_at, updated_at
+	FROM platform.parts
+	WHERE id=$1
+	`
+	row := r.pool.QueryRow(ctx, query, id)
+
+	var model partModel
+	if err := model.scan(row); err != nil {
+		if errors.Is(err, postgrespool.ErrNoRows) {
+			return domain.Part{}, fmt.Errorf("part with id='%s': %w", id, domain.ErrNotFound)
+		}
+
+		return domain.Part{}, fmt.Errorf("scan part row: %w", err)
+	}
+
+	return partModelToDomain(model), nil
+}
---
+++ inventory/internal/repository/part/repository.go
@@ -0,0 +1,15 @@
+package part
+
+import (
+	"github.com/horizoonn/factory-platform.git/platform/pkg/database/postgres/pool"
+)
+
+type Repository struct {
+	pool postgrespool.Pool
+}
+
+func NewRepository(pool postgrespool.Pool) *Repository {
+	return &Repository{
+		pool: pool,
+	}
+}
