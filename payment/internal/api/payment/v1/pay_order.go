8742ac3c9c60af9e51a2d3fe47b5aaed0d2fc5ce
horizon <iliasyunusov@yandex.ru>
2026-06-28 19:10:48 +0500

    feat(payment): add grpc api

---
+++ payment/internal/api/payment/v1/pay_order.go
@@ -0,0 +1,63 @@
+package paymentv1
+
+import (
+	"context"
+	"errors"
+
+	"github.com/google/uuid"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	"github.com/horizoonn/factory-platform.git/payment/internal/domain"
+	paymentservice "github.com/horizoonn/factory-platform.git/payment/internal/service"
+	paymentpb "github.com/horizoonn/factory-platform.git/shared/pkg/proto/payment/v1"
+)
+
+func (s *PaymentServer) PayOrder(ctx context.Context, req *paymentpb.PayOrderRequest) (*paymentpb.PayOrderResponse, error) {
+	orderID, err := uuid.Parse(req.GetOrderUuid())
+	if err != nil {
+		return nil, status.Errorf(codes.InvalidArgument, "invalid order uuid")
+	}
+
+	userID, err := uuid.Parse(req.GetUserUuid())
+	if err != nil {
+		return nil, status.Errorf(codes.InvalidArgument, "invalid user uuid")
+	}
+
+	paymentMethod, err := paymentMethodToDomain(req.GetPaymentMethod())
+	if err != nil {
+		if req.GetPaymentMethod() == paymentpb.PaymentMethod_PAYMENT_METHOD_UNSPECIFIED {
+			return nil, status.Error(codes.InvalidArgument, "payment_method is required")
+		}
+
+		return nil, status.Error(codes.InvalidArgument, "invalid payment_method")
+	}
+
+	result, err := s.paymentService.PayOrder(ctx,
+		paymentservice.PayOrderRequest{
+			OrderID:       orderID,
+			UserID:        userID,
+			PaymentMethod: paymentMethod,
+		})
+	if err != nil {
+		if errors.Is(err, domain.ErrInvalidPaymentMethod) ||
+			errors.Is(err, domain.ErrUserIDRequired) ||
+			errors.Is(err, domain.ErrOrderIDRequired) {
+			return nil, status.Error(codes.InvalidArgument, err.Error())
+		}
+
+		if errors.Is(err, context.Canceled) {
+			return nil, status.Error(codes.Canceled, err.Error())
+		}
+
+		if errors.Is(err, context.DeadlineExceeded) {
+			return nil, status.Error(codes.DeadlineExceeded, err.Error())
+		}
+
+		return nil, status.Error(codes.Internal, "internal error")
+	}
+
+	return &paymentpb.PayOrderResponse{
+		TransactionUuid: result.TransactionID.String(),
+	}, nil
+}
